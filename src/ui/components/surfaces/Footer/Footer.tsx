ed659070b29a0b53671c85843d0f96b37540f36b
AlefAdonis <alefadonis10@gmail.com>
2021-06-15 00:33:19 -0300

    Adicionando Footer, uma Mask para busca de Cep e funcionalidades logicas

---
+++ src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ marginTop: "16px" }}>
+            E-Diaristas te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza da sua casa. Garantimos a melhor profissional com
+            total segurança e praticidade! São milhares de clientes satisfeitos
+            por todo o país.
+          </Typography>
+        </Box>
+
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener norefeer"}>
+                <img src="img/logos/app-store.png" alt="AppStore" />
+              </a>
+            </li>
+
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener norefeer"}>
+                <img src="img/logos/google-play.png" alt="Google Play" />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
